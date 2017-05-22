1c9577396496884dfcb4514cce6c87fd94b37b14
ysard <ysard_git@gmx.fr>
2017-05-22 16:46:01 +0200

    Anti PEP 8 changes: Good indentation to bad indentation

--- src/__main__.py
+++ src/__main__.py
@@ -6,27 +6,27 @@ from src.commons import DIR_PICKLE, DIR_OUTPUT
 
 
 if __name__ == "__main__" :
-    parser = argparse.ArgumentParser(
-        description='biopax2cabiom.py is a script to transforme a Biopax data \
-        from a RDBMS to a Cabiom model.'
-    )
-    parser.add_argument('--pickleBackup', type=str, nargs='?',
-                        default=DIR_PICKLE + 'backup.p',
-                        help='enter a file path to save the script variables.'
-    )
-    parser.add_argument('--listOfGraphUri', nargs='+',
-                        help='enter a list of RDF graph.'
-    )
-    parser.add_argument('--cadbiomFile', type=str, nargs='?',
-                        default=DIR_OUTPUT + 'model.bcx',
-                        help='enter a file path to generate the Cadbiom model.'
-    )
-    parser.set_defaults(func=main)
+	parser = argparse.ArgumentParser(
+		description='biopax2cabiom.py is a script to transforme a Biopax data \
+		from a RDBMS to a Cabiom model.'
+	)
+	parser.add_argument('--pickleBackup', type=str, nargs='?',
+						default=DIR_PICKLE + 'backup.p',
+						help='enter a file path to save the script variables.'
+	)
+	parser.add_argument('--listOfGraphUri', nargs='+',
+						help='enter a list of RDF graph.'
+	)
+	parser.add_argument('--cadbiomFile', type=str, nargs='?',
+						default=DIR_OUTPUT + 'model.bcx',
+						help='enter a file path to generate the Cadbiom model.'
+	)
+	parser.set_defaults(func=main)
 
-    # get program args and launch associated command
-    args = parser.parse_args()
+	# get program args and launch associated command
+	args = parser.parse_args()
 
-    # Set log level
-    #LOGGER.setLevel(cm.LOG_LEVELS[vars(args)['verbose']])
-    # launch associated command
-    args.func(args)
+	# Set log level
+	#LOGGER.setLevel(cm.LOG_LEVELS[vars(args)['verbose']])
+	# launch associated command
+	args.func(args)
--- src/commons.py
+++ src/commons.py
@@ -23,22 +23,22 @@ SPARQL_PATH         = "https://openstack-192-168-100-241.genouest.org/sparql/"
 ################################################################################
 
 def logger(name=LOGGER_NAME, logfilename=None):
-    """Return logger of given name, without initialize it.
+	"""Return logger of given name, without initialize it.
 
-    Equivalent of logging.getLogger() call.
-    """
-    return logging.getLogger(name)
+	Equivalent of logging.getLogger() call.
+	"""
+	return logging.getLogger(name)
 
 _logger = logging.getLogger(LOGGER_NAME)
 _logger.setLevel(LOG_LEVEL)
 
 # log file
-formatter    = logging.Formatter(
-    '%(asctime)s :: %(levelname)s :: %(message)s'
+formatter = logging.Formatter(
+	'%(asctime)s :: %(levelname)s :: %(message)s'
 )
 file_handler = RotatingFileHandler(
-    DIR_LOGS + LOGGER_NAME + '.log',
-    'a', 10000000, 1
+	DIR_LOGS + LOGGER_NAME + '.log',
+	'a', 10000000, 1
 )
 file_handler.setLevel(LOG_LEVEL)
 file_handler.setFormatter(formatter)
@@ -52,42 +52,9 @@ stream_handler.setLevel(LOG_LEVEL)
 _logger.addHandler(stream_handler)
 
 def log_level(level):
-    """Set terminal log level to given one"""
-    handlers = (_ for _ in _logger.handlers
-                if _.__class__ is logging.StreamHandler
-               )
-    for handler in handlers:
-        handler.setLevel(level.upper())
-
-
-# Some convenient functions
-def chunk_this(iterable, length):
-    """Split iterable in chunks of equal sizes"""
-    iterator = iter(iterable)
-
-    # For dictionnaries
-    if (type(iterable) == type(dict())) or (type(iterable) == type(defaultdict())):
-        # + 1: adjust length
-        for i in range(0, len(iterable), length):
-            yield {k:iterable[k] for k in it.islice(iterator, length + 1)}
-    else:
-        # For (all) other iterables (?)
-        while True:
-            # + 1: adjust length
-            chunk = tuple(it.islice(iterator, length + 1))
-            if not chunk:
-               return
-            yield chunk
-
-
-def merge_dicts(*dict_args):
-    """Given any number of dicts, shallow copy and merge into a new dict,
-    precedence goes to key value pairs in latter dicts.
-    """
-    # Syntax for Python 3.5
-    # result = {**dict1, **dict2}
-    result = {}
-    for dictionary in dict_args:
-        result.update(dictionary)
-    return result
-
+	"""Set terminal log level to given one"""
+	handlers = (_ for _ in _logger.handlers
+				if _.__class__ is logging.StreamHandler
+			   )
+	for handler in handlers:
+		handler.setLevel(level.upper())
