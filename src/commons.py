d50958bba29c9a1853da8e0ab3e74fdcf70e8789
VIGNET Pierre <pierre.vignet@irisa.fr>
2017-06-15 15:51:21 +0200

    Clean file

--- src/commons.py
+++ src/commons.py
@@ -3,8 +3,6 @@
 # Standard imports
 from logging.handlers import RotatingFileHandler
 import logging
-import itertools as it
-from collections import defaultdict
 
 # Paths
 DIR_LOGS            = 'logs/'
@@ -25,22 +23,22 @@ SPARQL_PATH         = "https://openstack-192-168-100-241.genouest.org/sparql/"
 ################################################################################
 
 def logger(name=LOGGER_NAME, logfilename=None):
-	"""Return logger of given name, without initialize it.
+    """Return logger of given name, without initialize it.
 
-	Equivalent of logging.getLogger() call.
-	"""
-	return logging.getLogger(name)
+    Equivalent of logging.getLogger() call.
+    """
+    return logging.getLogger(name)
 
 _logger = logging.getLogger(LOGGER_NAME)
 _logger.setLevel(LOG_LEVEL)
 
 # log file
 formatter = logging.Formatter(
-	'%(asctime)s :: %(levelname)s :: %(message)s'
+    '%(asctime)s :: %(levelname)s :: %(message)s'
 )
 file_handler = RotatingFileHandler(
-	DIR_LOGS + LOGGER_NAME + '.log',
-	'a', 10000000, 1
+    DIR_LOGS + LOGGER_NAME + '.log',
+    'a', 10000000, 1
 )
 file_handler.setLevel(LOG_LEVEL)
 file_handler.setFormatter(formatter)
@@ -52,11 +50,3 @@ formatter      = logging.Formatter('%(levelname)s: %(message)s')
 stream_handler.setFormatter(formatter)
 stream_handler.setLevel(LOG_LEVEL)
 _logger.addHandler(stream_handler)
-
-def log_level(level):
-	"""Set terminal log level to given one"""
-	handlers = (_ for _ in _logger.handlers
-				if _.__class__ is logging.StreamHandler
-			   )
-	for handler in handlers:
-		handler.setLevel(level.upper())
